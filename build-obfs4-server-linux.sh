913e3ae01d7d20dcc337d742ca6b6cd3f25fa0d2
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-04-18 21:51:23 +0000

    Added build scripts

---
+++ build-obfs4-server-linux.sh
@@ -0,0 +1,20 @@
+export BASE=github.com/OperatorFoundation
+export SSPATH=Shapeshifter-obfs4-OpenVPN-Transport-Plugin-server
+export CPATH=$SSPATH-C
+export CGOPATH=$SSPATH-Cgo
+
+pushd .
+cd
+mkdir go
+cd go
+go get $BASE/$CGOPATH
+cd src/$BASE/$CGOPATH
+popd
+
+cp ~/go/src/$BASE/$CGOPATH/$CGOPATH.h transports/obfs4/$CPATH/include/shapeshifter-obfs4-server-go.h
+cp ~/go/src/$BASE/$CGOPATH/$CGOPATH.a transports/obfs4/$CPATH/lib/shapeshifter-obfs4-server-go.a
+
+cd transports/obfs4/$CPATH
+cmake .
+make
+
---
+++ build-openvpn-windows.sh
@@ -0,0 +1,4 @@
+git submodule update
+cd openvpn-build/windows-nsis
+./build-snapshot
+
