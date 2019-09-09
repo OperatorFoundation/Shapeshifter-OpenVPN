42f7bccf920ceadffde8c9aaebcdcdefe8399373
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-09-09 15:22:53 -0500

    Fix paths

--- scripts/server-linux/build-plugin.sh
+++ scripts/server-linux/build-plugin.sh
@@ -10,10 +10,10 @@ pushd "$GOPATH/src/$BASE/$1" || exit
 go build -buildmode=c-archive
 popd || exit
 
-cp "$GOPATH/src/$BASE/$1/$1.h" "plugins/$1/server-linux/shapeshifter-obfs4-server-1.0/include/shapeshifter-$1-go.h"
-cp "$GOPATH/src/$BASE/$1/$1.a" "plugins/$1/server-linux/shapeshifter-obfs4-server-1.0/lib/shapeshifter-$1-go.a"
+cp "$GOPATH/src/$BASE/$1/$1.h" "plugins/$1/server-linux/shapeshifter-$1-server-1.0/include/shapeshifter-$1-go.h"
+cp "$GOPATH/src/$BASE/$1/$1.a" "plugins/$1/server-linux/shapeshifter-$1-server-1.0/lib/shapeshifter-$1-go.a"
 
-cd "plugins/$1/server-linux/shapeshifter-obfs4-server-1.0" || exit
+cd "plugins/$1/server-linux/shapeshifter-$1-server-1.0" || exit
 cmake .
 make
 
