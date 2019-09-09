904bb20574d5de5593c6bab1a23208ae04c035be
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-09-09 13:55:22 -0500

    Moving plugins out of submodule and into tree (Step 2)

---
+++ plugins/shadow/client-windows/src/shapeshifter-shadow.h
@@ -0,0 +1,20 @@
+#ifndef OPENVPN_PLUGIN_shapeshifter_shadow_H
+#define OPENVPN_PLUGIN_shapeshifter_shadow_H 1
+
+#include "openvpn-plugin.h"
+#include "openvpn-vsocket.h"
+
+#define shapeshifter_shadow_PLUGIN_NAME "shapeshifter-shadow"
+
+struct shapeshifter_shadow_context
+{
+    struct openvpn_plugin_callbacks *global_vtab;
+    char *password;
+    char *cipherName;
+};
+
+extern struct openvpn_vsocket_vtab shapeshifter_shadow_socket_vtab;
+void shapeshifter_shadow_initialize_socket_vtab(void);
+void shapeshifter_shadow_log(struct shapeshifter_shadow_context *ctx, openvpn_plugin_log_flags_t flags, const char *fmt, ...);
+
+#endif /* !OPENVPN_PLUGIN_shapeshifter_shadow_H */
