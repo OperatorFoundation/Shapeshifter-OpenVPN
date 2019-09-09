049fd918d63e7072975556561035b4b6997beda9
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-09-09 14:33:25 -0500

    Style improvements

--- scripts/server-linux/build-plugin.sh
+++ scripts/server-linux/build-plugin.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-export GOROOT=$PWD/go-build
-export BASE=github.com/OperatorFoundation/Shapeshifter-OpenVPN
+export GOPATH="$PWD/go-build"
+export BASE=github.com/OperatorFoundation/Shapeshifter-OpenVPN/go
 
-mkdir -p $GOROOT
-pushd $GOROOT
-go get $BASE/$1
-popd
-pushd $GOROOT/src/$BASE/$1
+mkdir -p "$GOPATH"
+pushd "$GOPATH" || exit
+go get "$BASE/$1"
+popd || exit
+pushd "$GOPATH/src/$BASE/$1" || exit
 go build -buildmode=c-archive
-popd
+popd || exit
 
-cp $GOROOT/src/$BASE/$1/$1.h plugins/$1/server-linux/include/shapeshifter-$1-go.h
-cp $GOROOT/src/$BASE/$1/$1.a plugins/$1/server-linux/lib/shapeshifter-$1-go.a
+cp "$GOPATH/src/$BASE/$1/$1.h" "plugins/$1/server-linux/include/shapeshifter-$1-go.h"
+cp "$GOPATH/src/$BASE/$1/$1.a" "plugins/$1/server-linux/lib/shapeshifter-$1-go.a"
 
-cd plugins/$1/server-linux
+cd "plugins/$1/server-linux" || exit
 cmake .
 make
 
