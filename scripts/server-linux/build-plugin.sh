8de2be2943183bd9ecd9e189efe400da12a0edc4
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-09-13 13:26:46 -0500

    Linux fixes

--- scripts/server-linux/build-plugin.sh
+++ scripts/server-linux/build-plugin.sh
@@ -4,14 +4,14 @@ export BASE=github.com/OperatorFoundation/Shapeshifter-OpenVPN/go
 
 mkdir -p "$GOPATH"
 pushd "$GOPATH" || exit
-go get -u "$BASE/$1"
+go get -u "$BASE/$1/server"
 popd || exit
-pushd "$GOPATH/src/$BASE/$1" || exit
-go build -buildmode=c-archive
+pushd "$GOPATH/src/$BASE/$1/server" || exit
+go build -o "$1.a" -buildmode=c-archive
 popd || exit
 
-cp "$GOPATH/src/$BASE/$1/$1.h" "plugins/$1/server-linux/shapeshifter-$1-server-1.0/include/shapeshifter-$1-go.h"
-cp "$GOPATH/src/$BASE/$1/$1.a" "plugins/$1/server-linux/shapeshifter-$1-server-1.0/lib/shapeshifter-$1-go.a"
+cp "$GOPATH/src/$BASE/$1/server/$1.h" "plugins/$1/server-linux/shapeshifter-$1-server-1.0/include/shapeshifter-$1-go.h"
+cp "$GOPATH/src/$BASE/$1/server/$1.a" "plugins/$1/server-linux/shapeshifter-$1-server-1.0/lib/shapeshifter-$1-go.a"
 
 cd "plugins/$1/server-linux/shapeshifter-$1-server-1.0" || exit
 cmake .
